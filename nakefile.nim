481546a561f37ed53a19930f652518d231e2cd53
Wink Saville <wink@saville.com>
2015-03-25 13:59:59 -0700

    Add nakefile.
    A nakefile is a nim build DSL.

---
+++ nakefile.nim
@@ -0,0 +1,35 @@
+import nake
+
+const
+  buildArtifacts = @["src/cmdargsopt", "src/nimcache", "tests/test", "tests/nimcache", "nimcache"]
+  buildFlags = "--hints:off --warnings:off"
+
+task defaultTask, "Compile and run the tests":
+  runTask "build-tests"
+  runTask "run-tests"
+
+task "build-tests", "Build the tests":
+  if shell(nimExe, "c", buildFlags, "tests/test.nim"):
+    echo "success"
+
+task "run-tests", "Run the tests":
+  discard shell("tests/test", "rt=3.0", "v=0")
+
+task "nake", "build the nakefile":
+  if not shell("nim c", buildFlags, " nakefile"):
+    echo "could not build nakefile"
+    quit 1
+  else:
+    removeDir "nimcache"
+    quit 0
+
+task "clean", "clean build artifacts":
+  for file in buildArtifacts:
+    try:
+      removeFile(file)
+    except OSError:
+      try:
+        removeDir(file)
+      except OSError:
+        echo "Could not remove: ", file, " ", getCurrentExceptionMsg()
+
